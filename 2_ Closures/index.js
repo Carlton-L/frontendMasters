83904fa3b90333159893531295776f7a16bc76e1
Carlton <Carlton.l@outlook.com>
2021-03-08 14:27:29 -0800

    Closures exercises 1-7

--- 2_ Closures/index.js
+++ 2_ Closures/index.js
@@ -1,18 +1,26 @@
 // CHALLENGE 1
-function createFunction() {}
+function createFunction() {
+  return function outputFunction() {
+    console.log('hello');
+  };
+}
 
 // /*** Uncomment these to check your work! ***/
-// const function1 = createFunction();
-// function1(); // => should console.log('hello');
+const function1 = createFunction();
+function1(); // => should console.log('hello');
 
 // CHALLENGE 2
-function createFunctionPrinter(input) {}
+function createFunctionPrinter(input) {
+  return function outputFunction() {
+    console.log(input);
+  };
+}
 
 // /*** Uncomment these to check your work! ***/
-// const printSample = createFunctionPrinter('sample');
-// printSample(); // => should console.log('sample');
-// const printHello = createFunctionPrinter('hello');
-// printHello(); // => should console.log('hello');
+const printSample = createFunctionPrinter('sample');
+printSample(); // => should console.log('sample');
+const printHello = createFunctionPrinter('hello');
+printHello(); // => should console.log('hello');
 
 // CHALLENGE 3
 function outer() {
@@ -31,60 +39,109 @@ const jasCounter = outer();
 // Before your do, guess what will be logged from each function call.
 
 // /*** Uncomment these to check your work! ***/
-// willCounter();
-// willCounter();
-// willCounter();
+willCounter();
+willCounter();
+willCounter();
 
-// jasCounter();
-// willCounter();
+jasCounter();
+willCounter();
 
-function addByX(x) {}
+function addByX(x) {
+  return function outputFunction(num) {
+    // console.log(num + x)
+    return num + x;
+  };
+}
 
 // /*** Uncomment these to check your work! ***/
-// const addByTwo = addByX(2);
-// addByTwo(1); // => should return 3
-// addByTwo(2); // => should return 4
-// addByTwo(3); // => should return 5
+const addByTwo = addByX(2);
+addByTwo(1); // => should return 3
+addByTwo(2); // => should return 4
+addByTwo(3); // => should return 5
 
-// const addByThree = addByX(3);
-// addByThree(1); // => should return 4
-// addByThree(2); // => should return 5
+const addByThree = addByX(3);
+addByThree(1); // => should return 4
+addByThree(2); // => should return 5
 
-// const addByFour = addByX(4);
-// addByFour(4); // => should return 8
-// addByFour(5); // => should return 9
+const addByFour = addByX(4);
+addByFour(4); // => should return 8
+addByFour(5); // => should return 9
 
 // CHALLENGE 4
-function once(func) {}
+function once(func) {
+  let val;
+  return function outputFunction(args) {
+    if (!val) {
+      val = func(args);
+    }
+    return val;
+  };
+}
 
 // /*** Uncomment these to check your work! ***/
-// const onceFunc = once(addByTwo);
-// console.log(onceFunc(4));  // => should log 6
-// console.log(onceFunc(10));  // => should log 6
-// console.log(onceFunc(9001));  // => should log 6
+const onceFunc = once(addByTwo);
+console.log(onceFunc(4)); // => should log 6
+console.log(onceFunc(10)); // => should log 6
+console.log(onceFunc(9001)); // => should log 6
 
 // CHALLENGE 5
-function after(count, func) {}
+function after(count, func) {
+  let i = 0;
+  return function outputFunction(args) {
+    if (i === count - 1) {
+      func(args);
+    } else {
+      i++;
+    }
+  };
+}
 
 // /*** Uncomment these to check your work! ***/
-// const called = function() { console.log('hello') };
-// const afterCalled = after(3, called);
-// afterCalled(); // => nothing is printed
-// afterCalled(); // => nothing is printed
-// afterCalled(); // => 'hello' is printed
+const called = function () {
+  console.log('hello');
+};
+const afterCalled = after(3, called);
+afterCalled(); // => nothing is printed
+afterCalled(); // => nothing is printed
+afterCalled(); // => 'hello' is printed
 
 // CHALLENGE 6
-function delay(func, wait) {}
+function delay(func, wait) {
+  let done = false;
+  setTimeout(function () {
+    done = true;
+  }, wait);
+  return function outputFunction() {
+    if (done) {
+      func();
+    }
+  };
+}
+
+const delayedHello = delay(called, 3000);
+delayedHello(); // prints nothing
+setTimeout(delayedHello, 1000); // prints nothing
+setTimeout(delayedHello, 4000); // prints 'hello' after 4 seconds
 
 // CHALLENGE 7
-function rollCall(names) {}
+function rollCall(names) {
+  let i = 0;
+  return function outputFunction() {
+    if (i < names.length) {
+      console.log(names[i]);
+      i++;
+    } else {
+      console.log('Everyone accounted for');
+    }
+  };
+}
 
 // /*** Uncomment these to check your work! ***/
-// const rollCaller = rollCall(['Victoria', 'Juan', 'Ruth'])
-// rollCaller() // => should log 'Victoria'
-// rollCaller() // => should log 'Juan'
-// rollCaller() // => should log 'Ruth'
-// rollCaller() // => should log 'Everyone accounted for'
+const rollCaller = rollCall(['Victoria', 'Juan', 'Ruth']);
+rollCaller(); // => should log 'Victoria'
+rollCaller(); // => should log 'Juan'
+rollCaller(); // => should log 'Ruth'
+rollCaller(); // => should log 'Everyone accounted for'
 
 // CHALLENGE 8
 function saveOutput(func, magicWord) {
