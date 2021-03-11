80ecce9fa241489d74974134f3d039743ae008a0
Carlton-L <carlton.l@outlook.com>
2021-03-11 14:02:45 -0800

    React Router

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,32 +1,24 @@
-import ReactDOM from 'react-dom'
-import Pet from './Pet'
-
-// const App = () => {
-//   return React.createElement("div", {}, [
-//     React.createElement("h1", {}, "Adopt Me!"),
-//     React.createElement(Pet, {
-//       name: "Luna",
-//       animal: "Dog",
-//       breed: "Havanese",
-//     }),
-//     React.createElement(Pet, {
-//       name: "Pepper",
-//       animal: "Bird",
-//       breed: "Cockatiel",
-//     }),
-//     React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" }),
-//   ]);
-// };
+import { render } from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
-  returnv(
+  return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese" />
-      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-      <Pet name="Beam" animal="Dog" breed="Wheaten Terrier" />
+      <Router>
+        <Switch>
+          <Route path="/details/:id">
+            <Details />
+          </Route>
+          <Route path="/">
+            <SearchParams />
+          </Route>
+        </Switch>
+      </Router>
     </div>
-  )
-}
+  );
+};
 
-ReactDOM.render(<App />, document.getElementById("root"));
+render(<App />, document.getElementById("root"));
