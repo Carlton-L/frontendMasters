ef67b10574c07a667fd7252b1d130d8a062ee71f
Carlton Lindsay <ashleighmiller@Ashleighs-MacBook-Pro.local>
2021-03-11 09:59:11 -0800

    JSX

---
+++ adopt-me/src/App.js
@@ -0,0 +1,32 @@
+import ReactDOM from 'react-dom'
+import Pet from './Pet'
+
+// const App = () => {
+//   return React.createElement("div", {}, [
+//     React.createElement("h1", {}, "Adopt Me!"),
+//     React.createElement(Pet, {
+//       name: "Luna",
+//       animal: "Dog",
+//       breed: "Havanese",
+//     }),
+//     React.createElement(Pet, {
+//       name: "Pepper",
+//       animal: "Bird",
+//       breed: "Cockatiel",
+//     }),
+//     React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" }),
+//   ]);
+// };
+
+const App = () => {
+  returnv(
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+      <Pet name="Beam" animal="Dog" breed="Wheaten Terrier" />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
